57ceabfada30e15318263dd6a2d326f2eefb7379
Shiva <sivar15racer@gmail.com>
2024-11-06 21:19:20 +0530

    CORS

---
+++ src/main/java/com/buildwithshiva/configure/WebConfig.java
@@ -0,0 +1,23 @@
+package com.buildwithshiva.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000", "https://shop.buildwithshiva.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
